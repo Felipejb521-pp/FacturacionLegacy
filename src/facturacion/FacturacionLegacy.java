ef2e36623733cb787461d092c4bd01f30c5e8ea7
Felipe <felipejb521@gmail.com>
2026-05-26 10:09:08 +0200

    git commit Hemos actualizado javadoc

--- src/facturacion/FacturacionLegacy.java
+++ src/facturacion/FacturacionLegacy.java
@@ -8,13 +8,15 @@ public class FacturacionLegacy {
     private static final double DESCUENTO_ESTANDAR = 0.05;
 
     /**
-     * 
+     * @author Felipe Jimenez, Joaquin Torrubia
      * @param importeBase Importe bruto de la factura antes de aplicar descuentos.
      * @param tipoCliente Categoría del cliente
      * @param esSocioVip Indica si el cliente VIP tiene descuento extra 
      * @return           Importe final con el descuento aplicado
      */
 
+    
+    
     public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
         if (importeBase <= 0) {
             return 0;
